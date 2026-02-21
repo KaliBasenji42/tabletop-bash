56afd8d31dca344c3eae8bdb6bc48965326cc41d
Jacob D - Laptop <code.strike366@passinbox.com>
2026-02-21 02:34:44 -0700

    Basic network (chat)

--- client/__main__.py
+++ client/__main__.py
@@ -19,6 +19,8 @@ import time
 import random
 import socket
 import threading
+import queue
+import selectors
 import logging
 
 logging.basicConfig(
@@ -35,24 +37,31 @@ logging.debug('New Run: ')
 host = '127.0.0.1' # Host IP
 port = 65432 # Host Port
 
+clientName = '' # Player name
+
 message = '' # Chat message to send
+chatLog = [] # Array of strings, containing the chat log
+# Note: Sent as string, split on '\n' at receive
 
 # Control
 
 spf = 1/30 # Second per Frames
 tick = 0 # Time ticker
 run = True # Run Main Loop
+frameTime = time.time() # Time frame started for spf
 
 selected = (0, 0) # Selected cell
 
-termSize = (0, 0) # Terminal dimensions
-
+termSize = (0, 0) # Terminal dimensions (Lines/Height, Columns/Width)
 screenWidth = 0 # Screen width, defined after window objects
 screenHeight = 0 # Screen height
 
 termTooSmall = True # Wether the terminal is too small
 
 # Game
+
+tableGrid = [] # Array of strings representing the look of the table
+tableState = [] # Array of objects on the table (sent from server, updated in thread)
 
 ### Functions ###
 
@@ -111,7 +120,32 @@ def readFiles():
   pass
   
 
+# Network
+
+def processMessage(message): # Processes network messages and updates game state
+  
+  # Chat
+  
+  global chatLog
+  
+  if len(message) >= 5:
+    if message[:5] == 'chat:': # If chat
+      
+      chatLog = message[5:].split('\\;') # Array of each row
+      
+      while len(chatLog) > chatWindow.height - 3: # While too long
+        chatLog.pop(0) # Remove oldest
+      
+      logging.debug('Chat Log:\n' + str(chatLog)) # Logging
+      
+  
+
 # output
+
+def renderTable(): # Takes tableState and redraws tableGrid for rendering
+  
+  pass
+  
 
 def render(stdscr): # Render screen
   
@@ -122,15 +156,30 @@ def render(stdscr): # Render screen
   for window in windows:
     window.renderBackground(stdscr)
   
-  # Debug
+  # Chat log
   
-  widthStr = ( # String for logging width
-    'Width: ' + str(screenWidth) +
-    ' of ' + str(termSize[0])
+  global chatLog
+  
+  for i in range(len(chatLog)):
+    
+    stdscr.addstr(chatWindow.y + i + 1, chatWindow.x + 1, chatLog[i])
+    
+  
+  stdscr.addstr( # Cursor
+    chatWindow.y + chatWindow.height - 2,
+    chatWindow.x + 1,
+    '> '
   )
+  
+  # Debug
   
   heightStr = ( # String for logging height
     'Height: ' + str(screenHeight) +
+    ' of ' + str(termSize[0])
+  )
+  
+  widthStr = ( # String for logging width
+    'Width: ' + str(screenWidth) +
     ' of ' + str(termSize[1])
   )
   
@@ -147,6 +196,8 @@ def render(stdscr): # Render screen
 
 ### Classes ###
 
+# Graphics
+
 class window:
   
   def __init__(self, x, y, width, height, title):
@@ -160,7 +211,7 @@ class window:
     self.title = title
     
   
-  def generateBackground(self): # Generate background away, set to self
+  def generateBackground(self): # Generate background array, set to self
     
     background = [] # Background array (each line)
     row = '' # Temp row string
@@ -213,8 +264,6 @@ class window:
     
   
 
-# Define Windows
-
 tableWindow = window(0, 0, 64, 32, '┐Table┌')
 chatWindow = window(64, 0, 64, 16, '┐Chat┌')
 contextWindow = window(64, 16, 64, 16, '┐Context/Inventory┌')
@@ -229,6 +278,121 @@ for window in windows: # Each window
   screenHeight = max(screenHeight, window.y + window.height) # Screen height
   
 
+# Network
+
+class server:
+  
+  def __init__(self, host, port, clientName, clientColor):
+    
+    # Variables
+    
+    self.run = True # Loop control
+    self.disconnect = False # Wether its connected
+    
+    self.name = clientName # Player name
+    self.color = clientColor # Display color
+    
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Socket client
+    self.host = host # Host IP
+    self.port = port # Port
+    self.addr = (self.host, self.port) # Full address
+    
+    # Connect
+    
+    self.sock.connect(self.addr)
+    self.sock.setblocking(True) # Blocking
+    
+    # Initial Message
+    
+    self.send(('join:' + self.name))
+    
+    # Thread
+    
+    self.queue = queue.Queue() # Queue to ensure all messages received
+    self.buffer = '' # Buffer to ensure message whole-ness
+    
+    self.thread = threading.Thread(target = self.recv, daemon = True)
+    self.thread.start()
+    
+  
+  def send(self, message): # Send to server
+    
+    logging.info('Sending: ' + message) # Logging
+    
+    try:
+      
+      self.sock.sendall((message + '\n').encode())
+      
+    except Exception:
+      
+      logging.exception("Send Error")
+      
+    
+  
+  def recv(self): # Receive from server
+    
+    while self.run:
+      
+      try:
+        
+        # Get data
+        
+        data = self.sock.recv(2048)
+        
+        # Disconnect
+        
+        if not data:
+          
+          self.disconnect = True
+          
+          logging.info('Disconnected from server: No data/clean') # Logging
+          
+          break # Break
+          
+        
+        # Set Queue
+        
+        self.buffer += data.decode()
+        
+        while "\n" in self.buffer: # While data not a complete message
+          
+          message, self.buffer = self.buffer.split('\n', 1) # Split on message end
+          
+          self.queue.put(message) # Push to queue
+          
+          logging.debug('Message from server: ' + message) # Logging
+          logging.debug('Queue Size: ' + str(self.queue.qsize())) # Logging
+          
+        
+      except OSError as e:
+        
+        self.disconnect = True
+        
+        logging.info('Disconnected from server\n' + str(e)) # Logging
+        
+        break # Break if shutdown
+        
+      
+    
+  
+  def close(self): # Close connection
+    
+    self.run = False # Stop recv loop
+    
+    try:
+      self.sock.shutdown(socket.SHUT_RDWR) # Force stop recv loop
+    except:
+      pass
+    
+    self.sock.close() # Close
+    
+    if self.thread.is_alive(): # If alive
+      self.thread.join() # End thread
+    
+    logging.debug('Closed server') # Logging
+    
+  
+
 ### Threads ###
 
 #inputThread = threading.Thread(target=inputThreadFunc)
@@ -236,16 +400,36 @@ for window in windows: # Each window
 
 ### Pre-Loop ###
 
+# Server
+
+host = input('Host IP: ') # Inputs
+port = strToPosInt(input('Port: '))
+clientName = input('Username: ')
+
+try:
+  
+  clientServer = server(host, port, clientName, '') # Try making server
+  
+except Exception as e:
+  
+  logging.exception('Connection Error') # Logging
+  
+  print('\033[97;41mConnection Error\033[0m\n' + str(e)) # Error message
+  
+  quit() # Quit
+  
+
+# Config
+
 try:
   
   readFiles() # Try reading files
   
 except Exception as e:
   
-  logging.exception(e) # Log
+  logging.exception('File Read Error') # Logging
   
-  # Error message
-  print('\033[97;41mCould Not Read File\033[0m')
+  print('\033[97;41mCould Not Read File\033[0m') # Error message
   
   quit() # Quit
   
@@ -275,6 +459,7 @@ def main(stdscr):
   global run
   global tick
   global spf
+  global frameTime
   global selected
   global message
   global termSize
@@ -283,38 +468,41 @@ def main(stdscr):
   
   while run:
     
+    # Server disconnect
+    
+    if clientServer.disconnect:
+      
+      run = False # Stop running
+      
+      logging.debug('Server Disconnected') # Logging
+      
+      break # Stop loop
+      
+    
     ### Clock ###
     
     tick += 1 # Iterate time ticker
     
-    time.sleep(spf)
+    elapsed = time.time() - frameTime # Time since last frame
+    time.sleep(max(0, spf - elapsed)) # Pause
+    frameTime = time.time() # Update frame time
+    
+    ### Network ###
+    
+    while not clientServer.queue.empty(): # While there are messages
+      
+      message = clientServer.queue.get() # Get message from queue
+      
+      processMessage(message) # Process & update
+      
     
     ### Rendering ###
     
-    termSize = ( # Terminal size
-      os.get_terminal_size().columns,
-      os.get_terminal_size().lines
-    )
+    termSize = stdscr.getmaxyx() # Terminal size (Lines/Height, Columns/Width)
     
     termTooSmall = False # Too small base case
-    
-    if termSize[0] < screenWidth : # Width too small
       
-      termTooSmall = True # Set too small
-      
-      stdscr.clear() # Clear
-      
-      text = ( # Output text
-        'Terminal width too small!\n' +
-        str(termSize[0]) + ' of ' +
-        str(screenWidth) + ' columns'
-      )
-      
-      stdscr.addstr(0, 0, text, curses.color_pair(3)) # Out
-      
-      stdscr.refresh() # Refresh
-      
-    elif termSize[1] < screenHeight: # Height too small
+    if termSize[0] < screenHeight: # Height too small
       
       termTooSmall = True # Set too small
       
@@ -322,14 +510,30 @@ def main(stdscr):
       
       text = ( # Output text
         'Terminal height too small!\n' +
-        str(termSize[1]) + ' of ' +
+        str(termSize[0]) + ' of ' +
         str(screenHeight) + ' lines'
       )
       
       stdscr.addstr(0, 0, text, curses.color_pair(3)) # Out
       
       stdscr.refresh() # Refresh
-    
+      
+    elif termSize[1] < screenWidth : # Width too small
+      
+      termTooSmall = True # Set too small
+      
+      stdscr.clear() # Clear
+      
+      text = ( # Output text
+        'Terminal width too small!\n' +
+        str(termSize[1]) + ' of ' +
+        str(screenWidth) + ' columns'
+      )
+      
+      stdscr.addstr(0, 0, text, curses.color_pair(3)) # Out
+      
+      stdscr.refresh() # Refresh
+      
     else: # Everything is fine
       
       try: # Try because resizing terminal acts funny
@@ -349,12 +553,11 @@ def main(stdscr):
     
     ### Key Input ###
     
-    try: key = chr(stdscr.getch())
-    except: key = ''
+    key = stdscr.getch()
     
     # Escape
     
-    if key.lower() == 'q':
+    if key == ord('q'):
       
       if termTooSmall: # Minimal if too small
         run = False
@@ -380,21 +583,21 @@ def main(stdscr):
     
     # Movements
     
-    elif key.lower() == 'd': selected = (selected[0] + 1, selected[1])
-    elif key.lower() == 'a': selected = (selected[0] - 1, selected[1])
-    elif key.lower() == 's': selected = (selected[0], selected[1] + 1)
-    elif key.lower() == 'w': selected = (selected[0], selected[1] - 1)
+    elif key == ord('d'): selected = (selected[0] + 1, selected[1])
+    elif key == ord('a'): selected = (selected[0] - 1, selected[1])
+    elif key == ord('s'): selected = (selected[0], selected[1] + 1)
+    elif key == ord('w'): selected = (selected[0], selected[1] - 1)
     
     # Other keys
     
-    elif key.lower() == 'h' and not termTooSmall: # Help
+    elif key == ord('h') and not termTooSmall: # Help
       
       stdscr.nodelay(False) # Block until input
       
       text = [ # Help text array
         'wasd: Move selected',
-        'q/Q: Quit',
-        'h/H: Help',
+        'q: Quit',
+        'h: Help',
         '',
         'Press any key to exit'
       ]
@@ -421,12 +624,18 @@ def main(stdscr):
       stdscr.nodelay(True) # Reset to non-blocking
       
     
-    elif key.lower() == 't' and not termTooSmall: # Chat
+    elif key == ord('t') and not termTooSmall: # Chat
       
       curses.echo() # Allow echo
       stdscr.nodelay(False) # Block until input
       
-      message = stdscr.getstr(0, 0, 16) # Get message
+      message = stdscr.getstr( # Get message
+        chatWindow.y + chatWindow.height - 2,
+        chatWindow.x + 3,
+        chatWindow.width - 4
+      ).decode()
+      
+      clientServer.send('msg:' + message) # Send
       
       curses.noecho() # Reset to no echo
       stdscr.nodelay(True) # Reset to non-blocking
@@ -434,14 +643,20 @@ def main(stdscr):
     
     # Clamp Movements
     
-    if key != '':
+    if key != -1: # If there was a keypress
+      
+      try: # Log key
+        logging.debug('Key: ' + chr(key) + ' (' + str(key) + ')') # Normal
+      except:
+        logging.debug('Key: Error (' + str(key) + ')') # Error
+      
+      # Selected position
       
       selected = (
         max(min(selected[0], tableWindow.width), 0),
         max(min(selected[1], tableWindow.height), 0)
       )
       
-      logging.debug('Key: ' + key) # Logging
       logging.debug('Selected: ' + str(selected))
       
     
@@ -453,7 +668,7 @@ try:
   curses.wrapper(main)
 except Exception as e:
   
-  logging.exception("Fatal Error") # Log
+  logging.exception('Fatal Error') # Log
   
   # Error message
   print('\033[97;41mFatal Error\033[0m')
@@ -461,6 +676,9 @@ except Exception as e:
 
 ### Post-Loop ###
 
-print('Bye 👋')
+if clientServer.disconnect: # Killed due to disconnect?
+  print('\033[97;41mDisconnected From Server\033[0m') # Print
 
-#inputThread.join()
+clientServer.close() # Close server
+
+print('Good Bye!')
--- host/__main__.py
+++ host/__main__.py
@@ -10,3 +10,344 @@
 # License: ./LICENSE.md
 # GPL v2: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 # KaliBasenji42's Github: https://github.com/KaliBasenji42
+
+### Imports ###
+
+import random
+import socket
+import threading
+import queue
+import logging
+
+logging.basicConfig(
+  level=logging.DEBUG,
+  format='%(asctime)s | %(levelname)s: %(message)s',
+  filename='app.log'
+)
+logging.debug('New Run: ')
+
+### Variables ###
+
+# Server
+
+host = '127.0.0.1' # Host IP
+port = 65432 # Host Port
+
+clients = [] # Array of client connections
+clientsLock = threading.Lock() # Thread lock for clients 
+# Lock ensures threads play nice with global value
+
+serverQueue = queue.Queue() # Queue to process all client messages
+
+usernames = {} # Dictionary of usernames
+
+# Control
+
+run = threading.Event() # Thread event, wether to run Main Loop
+run.set()
+
+# Data (sent stuff)
+
+chatLog = [] # Array of tuples of strings (user, message, type), containing the chat log
+# Note: Sent as string, split on '\\;' at receive
+tableState = [] # Array of objects on the table (sent from server, updated in thread)
+
+chatLength = 20 # Max chat log length
+
+### Functions ###
+
+# Basic
+
+def strToPosInt(string): # Converts string to positive integer, returns 0 if no number, ignores non-numeric characters
+  
+  numStr = ''
+  
+  for i in range(len(string)):
+    if string[i].isnumeric(): numStr = numStr + string[i]
+  
+  if len(numStr) == 0: return 0
+  
+  else: return int(numStr)
+  
+
+def strToFloat(string): # Converts string to float, returns 0 if no number, ignores non-numeric characters
+  
+  numStr = ''
+  
+  for i in range(len(string)):
+    
+    if string[i].isnumeric(): numStr = numStr + string[i]
+    if string[i] == '.': numStr = numStr + string[i]
+    
+  
+  if len(numStr) == 0: return 0
+  
+  if string[0] == '-': return -1 * float(numStr)
+  else: return float(numStr)
+  
+
+def hasNumerics(string): # Returns wether a string has numeric characters
+  
+  for char in string:
+    if char.isnumeric(): return True
+  
+  return False
+  
+
+def roll(arr, new): # Have an array roll in a new value, removing the first
+  
+  out = arr
+  
+  out.append(new) # Add new
+  out.pop(0) # Remove first
+  
+  return out
+  
+
+# Network
+
+def addClient(conn): # Add client to clients array
+  
+  with clientsLock:
+    
+    clients.append(conn) # Add
+    
+  
+
+def removeClient(conn): # Remove client from clients array
+  
+  with clientsLock:
+    
+    if conn in clients: # If it exists
+      
+      clients.remove(conn) # Remove
+      
+    
+  
+
+def broadcast(message): # Send message to all connections
+  
+  with clientsLock:
+    
+    for conn in clients.copy(): # For each client (copy to avoid disconnect error)
+      
+      try:
+        
+        conn.sendall((message + '\n').encode()) # Send
+        
+      except Exception as e:
+        
+        logging.exception('Failed to send message to ' + str(conn)) # Logging
+        
+        removeClient(conn) # Remove client
+        
+      
+    
+  
+
+def generateChatLog(chatLog): # Generate string to send for chat log
+  
+  global usernames
+  
+  out = '' # Output string
+  
+  while len(chatLog) > chatLength: # While too long
+    chatLog.pop(0) # Remove oldest
+  
+  for log in chatLog: # Each log
+    
+    if log[2] == 'conn': # Type Connection
+      out += usernames.get(log[0], log[0]) + ' ' # Add username
+      out += log[1] + '\\;' # Add message
+    
+    elif log[2] == 'msg': # Type Message
+      out += usernames.get(log[0], log[0]) + ':\\;> ' # Add username
+      out += log[1] + '\\;' # Add message
+    
+  
+  logging.debug('Chat log:\n' + str(chatLog))
+  
+  return out # Return
+  
+
+### (Other) Threads ###
+
+# Server Queue Function
+
+def serverQueueThreadFunction(): # Processes server queue messages
+  
+  # Pre-Loop
+  
+  global chatLog
+  
+  # Main Loop
+  
+  while run.is_set():
+    
+    try:
+      
+      # Get message
+      
+      addr, message = serverQueue.get(timeout = 1)
+      
+      # Disconnect
+      
+      if message == 'disconnect':
+        
+        chatLog.append((addr, 'Left', 'conn')) # Chat log
+        broadcast('chat:' + generateChatLog(chatLog))
+        
+      
+      # Join
+      
+      if len(message) >= 5:
+        if message[:5] == 'join:': # If join
+          
+          usernames[addr] = message[5:] # Add username
+          
+          logging.debug('Usernames:\n' + str(usernames)) # Logging
+          
+          chatLog.append((addr, 'Joined', 'conn')) # Chat log
+          broadcast('chat:' + generateChatLog(chatLog))
+          
+      
+      # Chat Message
+      
+      if len(message) >= 4:
+        if message[:4] == 'msg:': # If chat message
+          
+          chatLog.append((addr, message[4:], 'msg')) # Chat log
+          broadcast('chat:' + generateChatLog(chatLog))
+          
+        
+      
+    except queue.Empty:
+      continue # Continue if empty
+    
+  
+
+# Server Queue Thread
+
+serverQueueThread = threading.Thread(target = serverQueueThreadFunction, daemon = True) # Declare
+serverQueueThread.start() # Start
+
+### Pre-Loop ###
+
+# Input
+
+host = input('Host IP: ')
+port = strToPosInt(input('Port: '))
+
+# Socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Declare socket
+
+try:
+  
+  sock.bind((host, port)) # Try bond
+  sock.listen(5) # Listen for incoming connections
+  
+  logging.info('Server Started') # Logging
+  print('Server Started!')
+  
+except Exception as e:
+  
+  logging.exception('Socket Bind Error') # Logging
+  print('\033[97;41mSocket Bind Error\033[0m\n' + str(e))
+  
+
+### Main Loop ###
+
+# Thread Function
+
+def clientThreadFunction(conn, addr):
+  
+  # Pre-Loop
+  
+  addClient(conn) # Add to clients array
+  
+  print('Connected to: ' + str(addr)) # Logging
+  logging.info('Connected to: ' + str(addr))
+  
+  conn.sendall('Welcome!\n'.encode()) # Test welcome message
+  
+  clientBuffer = '' # Buffer to ensure message whole-ness
+  
+  addrStr = addr[0] + ':' + str(addr[1]) # Address as string
+  
+  # Main Loop
+  
+  while run.is_set():
+    
+    try:
+      
+      # Get data
+      
+      data = conn.recv(2048)
+      
+      # Disconnect
+      
+      if not data:
+        
+        print('Disconnected from: ' + str(addr) + ' No data/clean') # Logging
+        logging.info('Disconnected from: ' + str(addr) + ' No data/clean')
+        
+        break # Break
+        
+      
+      # Set Queue
+      
+      clientBuffer += data.decode()
+      
+      while "\n" in clientBuffer: # While data not a complete message
+        
+        message, clientBuffer = clientBuffer.split('\n', 1) # Split on message end
+        
+        serverQueue.put((addrStr, message)) # Push to queue
+        
+        logging.debug('Message from ' + str(addr) + ': ' + message) # Logging
+        
+      
+      # Process
+      
+      
+    except Exception as e:
+      
+      print('Disconnected from: ' + str(addr) + '\n' + str(e)) # Logging
+      logging.info('Disconnected from: ' + str(addr) + '\n' + str(e))
+      
+      break # Exit loop
+      
+    
+  
+  removeClient(conn) # Remove from clients array
+  
+  serverQueue.put((addrStr, 'disconnect')) # Tell server queue
+  
+  conn.close() # Close
+  
+
+# Try
+
+try:
+  
+  while run.is_set():
+    
+    conn, addr = sock.accept() # Accept incoming
+    
+    thread = threading.Thread(target = clientThreadFunction, args = (conn, addr), daemon = True)
+    thread.start() # Thread per client
+    
+  
+except KeyboardInterrupt: # Shutdown
+  
+  logging.info('Keyboard Interrupt') # Logging
+  print('\033[92mShutting Down\033[0m')
+  
+  run.clear() # Stop threads
+  
+
+### Post-Loop ###
+
+sock.close() # Close socket
+
